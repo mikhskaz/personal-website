088debf45882977fe8d50d6ecb6936828d195dfb
Mikhail Skazhenyuk <mikhska@gmail.com>
2025-07-07 13:39:25 -0300

    Remove cursor effect on touch screens

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-// import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import Footer from './components/Footer'
 import NavBar from './components/Navbar'
 import Hero from './components/Hero'
@@ -10,9 +10,21 @@ import Skills from './components/Skills'
 
 function App() {
 
+  const [isTouchDevice, setIsTouchDevice] = useState<boolean>(false);
+
+  useEffect(() => {
+    const isTouch =
+      'ontouchstart' in window ||
+      navigator.maxTouchPoints > 0 ||
+      // For older IE devices (optional)
+      // @ts-ignore
+      navigator.msMaxTouchPoints > 0;
+    setIsTouchDevice(isTouch);
+  }, []);
+
   return (
     <div>
-      <CursorEffect />
+      {!isTouchDevice && <CursorEffect />}
       {/* <main className="relative min-h-screen w-screen overflow-x-hidden"> */}
       <NavBar />
       <Hero />
