670f1b44fbdfd4740015ae4d81c267c4d1f38507
PulchritudinousHaese <micah.skazhenyuk@mail.utoronto.ca>
2026-02-02 00:07:22 -0500

    Additional iOS fix

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -26,11 +26,11 @@ const Hero = () => {
     return (
         <div className="relative h-svh w-full">
             <div className="relative z-20 h-svh rounded-lg">
-                <section id="home" className="hero-section inset-0 z-10 flex flex-col justify-between h-svh text-white">
+                <section id="home" className="hero-section inset-0 z-10 h-svh text-white">
                 <div className="fixed top-0 left-0 w-full h-full z-0 pointer-events-none">
                     <div className="hero-bg absolute w-full h-full bg-gradient-to-t from-primary to-secondary" />
                     </div>
-                    <div className="z-40">
+                    <div className="absolute left-0 top-0 z-40">
                         <div className="mt-30 ml-10">
                             <h1 className="hero-heading">Mikhail</h1>
                             {/* <Button
@@ -57,21 +57,21 @@ const Hero = () => {
                             {/* <p className="hero-text ml-1"></p> */}
                         </div>
                     </div>
-                    <div className="z-40 text-right pr-5 pb-5">
-                        <h1 className="quote-text font-display italic text-white leading-relaxed mb-2">
+                    <div className="absolute inset-0 z-40 flex flex-col justify-end items-end pr-5 pb-5">
+                        <h1 className="quote-text font-display italic text-white leading-relaxed text-right mb-2">
                             "Computer science is no more about computers than astronomy is about telescopes, biology is about microscopes or chemistry is about beakers and test tubes. Science is not about tools. It is about how we use them, and what we find out when we do."
                         </h1>
-                        <h1 className="quote-author max-w-md font-display font-bold text-white ml-auto">
+                        <h1 className="quote-author max-w-md font-display font-bold text-white text-right">
                             - Edsger W. Dijkstra
                         </h1>
                     </div>
                 </section>
             </div>
-            <div className="absolute inset-0 flex flex-col justify-end text-right pr-5 pb-5">
-                <h1 className="quote-text font-display italic text-secondary leading-relaxed mb-2">
+            <div className="absolute inset-0 flex flex-col justify-end items-end pr-5 pb-5">
+                <h1 className="quote-text font-display italic text-secondary leading-relaxed text-right mb-2">
                     "Computer science is no more about computers than astronomy is about telescopes, biology is about microscopes or chemistry is about beakers and test tubes. Science is not about tools. It is about how we use them, and what we find out when we do."
                 </h1>
-                <h1 className="quote-author max-w-md font-display font-bold text-secondary ml-auto">
+                <h1 className="quote-author max-w-md font-display font-bold text-secondary text-right">
                     - Edsger W. Dijkstra
                 </h1>
             </div>
